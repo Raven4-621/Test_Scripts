db048361e2f30a6e1c21739febd0d7d021b544bc
Heathcliff <Raven4-621@outlook.com>
2024-01-14 23:24:15 +0800

    /

--- py/temp.py
+++ py/temp.py
@@ -3,6 +3,9 @@
 # Author: V22026851
 
 import uiautomator2 as u2
+import time as t
 
-device = u2.connect("192.168.1.6")
-print(device.device_info)
+d = u2.connect("192.168.1.4")
+d.unlock()
+t.sleep(1)
+d.swipe_points([(250, 1270), (555, 1570), (850, 1890), (865, 1570), (535, 1300), (260, 1565), (265, 1870), (850, 1275)], 0.2)
