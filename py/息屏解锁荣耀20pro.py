7fe3bbc4fe7e19a0e416f5197b24796fa5353b63
Heathcliff <Raven4-621@outlook.com>
2024-01-16 00:38:48 +0800

    /

--- py/息屏解锁荣耀20pro.py
+++ py/息屏解锁荣耀20pro.py
@@ -5,7 +5,7 @@
 import uiautomator2 as u2
 import time as t
 
-d = u2.connect("192.168.1.4")
+d = u2.connect()
 d.unlock()
 t.sleep(1)
 d.swipe_points([(250, 1270), (555, 1570), (850, 1890), (865, 1570), (535, 1300), (260, 1565), (265, 1870), (850, 1275)], 0.1)
